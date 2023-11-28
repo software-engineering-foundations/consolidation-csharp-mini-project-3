2626ab05c7f729f0af2f4e91cc132e983ff3529b
Emily Flynn <emily.flynn@softwire.com>
2023-11-28 14:08:12 +0000

    Remove Python references

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,26 +1,9 @@
-namespace Calculator
+namespace Calculator;
+
+public class Calculator
 {
-    public class Calculator
+    public double Calculate(string expression)
     {
-        private Dictionary<string, Func<double, double, double>> operators;
-
-        public Calculator()
-        {
-            operators = new Dictionary<string, Func<double, double, double>>
-            {
-                { "+", (a, b) => a + b },
-                { "-", (a, b) => a - b },
-                { "*", (a, b) => a * b },
-                { "**", Math.Pow },
-                { "/", (a, b) => a / b },
-                { "//", (a, b) => Math.Floor(a / b) },
-                { "%", (a, b) => a % b }
-            };
-        }
-
-        public double Calculate(string expression)
-        {
-            return 0;
-        }
+        return 0;
     }
 }
